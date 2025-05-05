283fdf55e323e13b6fc9cb6078e18ed7d6c69519
David Buchaca <dbuchaca@amazon.com>
2025-05-06 00:53:32 +0200

    feat: add notebook showcasing table indexer

--- lsearch/__init__.py
+++ lsearch/__init__.py
@@ -8,6 +8,7 @@ from . import table_serializer
 from .inverted_index.inverted_index import InvertedIndex
 #from .text_processing.tokenization import tokenizers
 from .table_serializer.table_serializer import TableSerializer
+from .table_indexer.table_indexer import TableIndexer
 
 # List all exposed modules (for documentation and `dir()`)
 __all__ = ['inverted_index', 'table_serializer', 'text_processing']
